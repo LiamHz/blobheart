a7b00d13eea3d4ca224486ddb2a458d49647d85e
LiamHz <liam.hinzman@gmail.com>
2020-12-05 16:00:56 -0700

    Improved fade down animation for hexagon grid

--- sketch.js
+++ sketch.js
@@ -34,7 +34,6 @@ function scene1() {
 
 HEXAGON_SIZE = 20 // edge size
 HEXAGON_HEIGHT = Math.sqrt(Math.pow(HEXAGON_SIZE, 2) - Math.pow(HEXAGON_SIZE/2, 2))
-GUTTER = 0.5
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL)
@@ -48,7 +47,16 @@ function windowResized() {
 hexColors = ['#9b5de5', '#f15bb5', '#fee440', '#00bbf9', '#00f5d4']
 hexColors = hexColors.map(x => hexToHSL(x))
 
-function hexGrid(fade, fadeType, fadeDir) {
+/*  
+1. Make "roling wave" fade down (nrows away from target row sets fade value)
+2. Make hexagons pulse by changing gutter size
+3. Make hexagons pulse by changing gutter size 
+4. Play around with stroke everywhere, but only some are filled in
+*/
+
+const clamp = (num, a, b) => Math.max(Math.min(num, Math.max(a, b)), Math.min(a, b));
+
+function hexGrid(fade, fadeType, fadeDir, gutter=0.5) {
   const hexChance = 0.7
   const gridHeight = windowHeight / (HEXAGON_HEIGHT) + 1
   const gridWidth = windowWidth / (HEXAGON_SIZE*3) + 1 
@@ -64,38 +72,40 @@ function hexGrid(fade, fadeType, fadeDir) {
     hexColor._array[3] = fade
     fill(hexColor)
     if (Math.random() > hexChance) {
-      polygon(0, 0, HEXAGON_SIZE - GUTTER, 6)
+      polygon(0, 0, HEXAGON_SIZE - gutter, 6)
     }
       
     pop()
   }
 
-  function getHexFade(idx) {
+  function getHexFade(rowN, targetRow) {
     hexFade = 0
     if (fadeType === "out") {
       hexFade = fade + randfloat(-0.35, 0)
     } else if (fadeType == "down") {
-      // Logging this significantly lowers FPS
-      //console.log((fade / gridHeight) / idx, "fades")
-      hexFade = (fade / gridHeight) / idx * 1600
+      hexFade = 1.0 - (Math.abs(rowN - targetRow) / gridHeight)
+      hexFade = Math.min(fade, hexFade)
     } else {
       console.error("invalid fade type: ", fadeType)
     }
 
     return hexFade
   }
-
+  // Row which is full opacity
+  // Opacity of hexagons is their distance from that row
+  targetRow = Math.floor(clamp(fade, 0, 1) * gridHeight)
+  console.log(targetRow)
   for (let i = 0; i < gridHeight; i++) {
     push()
     // % 2 is used because hexagons alternate where they start from
     translate(-windowWidth/2  + HEXAGON_SIZE*(3/2*(i%2)),
               -windowHeight/2 + HEXAGON_HEIGHT*(i))
 
-    drawHex(getHexFade(i))
+    drawHex(getHexFade(i, targetRow))
 
     for (let j = 0; j < gridWidth - 1; j++){
       translate(p5.Vector.fromAngle(radians(0), HEXAGON_SIZE * 3))
-      drawHex(getHexFade(i))
+      drawHex(getHexFade(i, targetRow))
     }
     pop()
   }
@@ -107,15 +117,16 @@ let fadeDir = "forward"
 
 let hex1Seed = 42
 let hex2Seed = 69
+let chill = 0.0
 
 function draw() {
   background(255)
-  fade += fadeDir == "forward" ? 0.01 : -0.01
+  fade += fadeDir == "forward" ? 0.005 : -0.005
 
-  if (fade <= -0.2) {
+  if (fade <= 0-chill) {
     fadeDir = fadeDir == "forward" ? "reverse" : "forward"
     hex1Seed += Math.random()
-  } else if (fade >= 1.2) {
+  } else if (fade >= 1+chill) {
     fadeDir = fadeDir == "forward" ? "reverse" : "forward"
     hex2Seed += Math.random()
   }
