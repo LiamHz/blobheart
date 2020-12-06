b3e34ead9ba04d4d66d0259398df8f6ee1def038
Carol Chen <hello@carolchen.me>
2020-12-06 14:50:49 -0500

    progress

--- bestaplaid.js
+++ bestaplaid.js
@@ -1,19 +1,20 @@
-const hexagonEdge = 250
-const hexagonHeight = Math.sqrt(Math.pow(hexagonEdge, 2) - Math.pow(hexagonEdge/2, 2))
-const extra = 100 // distance for line to extend out of screen
-
 // TODO:
 // - bezier curved speed changes
 // - add striped lines
 
-const leftPoint = makePoint(-hexagonEdge, 0)
-const rightPoint = makePoint(hexagonEdge, 0)
 let seed = Math.random()
 
 function bestaplaid(time) {
+  let hexagonEdge = Math.min(windowWidth, windowHeight)/5
+  let hexagonHeight = Math.sqrt(Math.pow(hexagonEdge, 2) - Math.pow(hexagonEdge/2, 2))
+  let extra = 100 // distance for line to extend out of screen
+  const leftPoint = makePoint(-hexagonEdge, 0)
+  const rightPoint = makePoint(hexagonEdge, 0)
+  const maxDistance = Math.max(windowWidth, windowHeight) / 2 + extra
+  const numLinesScale = Math.max(windowWidth, windowHeight) / 900
   let msPerLine = 500
   let msOffset = 0
-
+  let speed = 0.7
   const lineStyles = [horizontalLines, forwardSlashLines, backwardSlashLines, diagonalLines, allLines]
 
   // handMadePurple()
@@ -33,206 +34,150 @@ function bestaplaid(time) {
 
   function timeScale() {
     scale = Math.min((time - msOffset) / msPerLine - 1, 1)
-    return scale
+    if (scale > 0) {
+      scale = 1/2*sin(scale*PI - PI/2) + 1/2
+    }
+    return Math.max(scale, -1)
   }
 
   function sleep(ms) {
     msOffset += ms
   }
 
-  function topLine() {
-    arguments[0] = 0 // cannot translate horizontal lines horizontally
-    lineTranslate.apply(null, arguments)
-    line(-windowWidth/2, -hexagonHeight, windowWidth * timeScale(), -hexagonHeight)
+  function drawLine(x1, y1, x2, y2, translations) {
+    lineTranslate.apply(null, translations)
+    if (binRand() || true) {
+      line(x1, y1, x2, y2)
+    } else {
+      drawDashedLine.apply(null, arguments)
+    }
     msOffset += msPerLine
     pop()
+  }
+
+  function drawDashedLine(x1, y1, x2, y2, translations) {
+    noStroke()
+    strokeCol = this._renderer.curStrokeColor
+    fillCol = `rgba(${strokeCol[0]*255}, ${strokeCol[1]*255}, ${strokeCol[2]*255}, ${strokeCol[3]})`
+    fill(color(fillCol))
+    height = this._renderer.curStrokeWeight
+
+    width = rand(5, height/10)
+    base = makePoint(x1 - width, y1)
+    i = 0
+
+    while (i < 100) {
+      quad(
+        base.x + height, base.y,
+        base.x + width, base.y-height,
+        base.x + 2*width, base.y-height,
+        base.x + width + height, base.y
+      )
+      msOffset += msPerLine
+      base.x += width * 2 * Math.max(-(x2 < x1), 1)
+      i += 1
+    }
+    pop()
+  }
+
+  function topLine() {
+    flip = chooseRand([1, -1])
+    arguments[0] = 0 // cannot translate horizontal lines horizontally
+    drawLine(-windowWidth/2*flip, -hexagonHeight, windowWidth * timeScale()*flip, -hexagonHeight, arguments)
   }
 
   function bottomLine() {
+    flip = chooseRand([1, -1])
     arguments[0] = 0 // cannot translate horizontal lines horizontally
-    lineTranslate.apply(null, arguments)
-    line(+windowWidth/2, hexagonHeight, -windowWidth * timeScale(), hexagonHeight)
-    msOffset += msPerLine
-    pop()
+    drawLine(+windowWidth/2*flip, hexagonHeight, -windowWidth * timeScale()*flip, hexagonHeight, arguments)
   }
 
   function topLeftLine() {
-    lineTranslate.apply(null, arguments)
-    line(
-      leftPoint.x - windowHeight/sqrt(3),
-      windowHeight + extra,
-      leftPoint.x + (windowHeight/sqrt(3)) * timeScale(),
-      (-windowHeight - extra) * timeScale()
+    flip = chooseRand([1, -1])
+    drawLine(
+      leftPoint.x - windowHeight/sqrt(3)*flip,
+      (windowHeight + extra)*flip,
+      leftPoint.x + (windowHeight/sqrt(3)) * timeScale() * flip,
+      (-windowHeight - extra) * timeScale() * flip,
+      arguments
     )
-    msOffset += msPerLine
-    pop()
   }
 
   function bottomLeftLine() {
-    lineTranslate.apply(null, arguments)
-    line(
-      leftPoint.x - windowHeight/sqrt(3),
-      -windowHeight - extra,
-      leftPoint.x + (windowHeight/sqrt(3)) * timeScale(),
-      (+windowHeight + extra) * timeScale()
+    flip = chooseRand([1, -1])
+    drawLine(
+      leftPoint.x - windowHeight/sqrt(3)*flip,
+      (-windowHeight - extra)*flip,
+      leftPoint.x + (windowHeight/sqrt(3)) * timeScale() * flip,
+      (+windowHeight + extra) * timeScale() * flip,
+      arguments
     )
-    msOffset += msPerLine
-    pop()
   }
 
   function topRightLine() {
-    lineTranslate.apply(null, arguments)
-    line(
-      rightPoint.x + windowHeight/sqrt(3),
-      +windowHeight + extra,
-      rightPoint.x - windowHeight/sqrt(3) * timeScale(),
-      (-windowHeight - extra) * timeScale()
+    flip = chooseRand([1, -1])
+    drawLine(
+      rightPoint.x + windowHeight/sqrt(3)*flip,
+      (windowHeight + extra)*flip,
+      rightPoint.x - windowHeight/sqrt(3) * timeScale() * flip,
+      (-windowHeight - extra) * timeScale() * flip,
+      arguments
     )
-    msOffset += msPerLine
-    pop()
   }
 
   function bottomRightLine() {
-    lineTranslate.apply(null, arguments)
-    line(
-      rightPoint.x + (windowHeight/sqrt(3)),
-      -windowHeight - extra,
-      rightPoint.x - windowHeight/sqrt(3) * timeScale(),
-      (windowHeight + extra) * timeScale()
+    flip = chooseRand([1, -1])
+    drawLine(
+      rightPoint.x + (windowHeight/sqrt(3))*flip,
+      (-windowHeight - extra) * flip,
+      rightPoint.x - windowHeight/sqrt(3) * timeScale()*flip,
+      (windowHeight + extra) * timeScale()*flip,
+      arguments
     )
-    msOffset += msPerLine
-    pop()
+  }
+
+  function drawTwoLines(x, y, x1, x2, y1, y2) {
+    // randomizes order and togetherness
+    if (binRand()){
+      x(x1, x2)
+      msOffset -= msPerLine * binRand()
+      y(y1, y2)
+    } else {
+      y(y1, y2)
+      msOffset -= msPerLine * binRand()
+      x(x1, x2)
+    }
   }
 
   function horizontalLines(distanceFromCenter, _) {
-    topLine(0, -distanceFromCenter)
-    msOffset -= msPerLine
-    bottomLine(0, distanceFromCenter)
+    drawTwoLines(topLine, bottomLine, 0, -distanceFromCenter, 0, distanceFromCenter)
   }
 
   function forwardSlashLines(distanceFromCenter, offset) {
-    topLeftLine(-distanceFromCenter, offset)
-    msOffset -= msPerLine
-    bottomRightLine(distanceFromCenter, offset)
+    drawTwoLines(topLeftLine, bottomRightLine, -distanceFromCenter, offset, distanceFromCenter, offset)
   }
 
   function backwardSlashLines(distanceFromCenter, offset) {
-    topRightLine(distanceFromCenter, offset)
-    msOffset -= msPerLine
-    bottomLeftLine(-distanceFromCenter, offset)
+    drawTwoLines(topRightLine, bottomLeftLine, distanceFromCenter, offset, -distanceFromCenter, offset)
   }
 
   function diagonalLines(distanceFromCenter, offset) {
-    backwardSlashLines(distanceFromCenter, offset)
-    msOffset -= msPerLine
-    forwardSlashLines(distanceFromCenter, offset)
+    drawTwoLines(backwardSlashLines, forwardSlashLines, distanceFromCenter, offset, distanceFromCenter, offset)
   }
 
   function allLines(distanceFromCenter, offset) {
-    horizontalLines(distanceFromCenter, offset)
-    msOffset -= msPerLine
-    diagonalLines(distanceFromCenter, offset)
-  }
-
-  function handMadePurple() {
-    const theme = themes[1]
-    background(theme[1])
-
-    // make hexagon black
-    noStroke()
-    fill(black)
-    polygon(0, 0, hexagonEdge, 6)
-
-    sleep(1000)
-
-    // innermost six lines, drawn one at a time
-    msPerLine = 600
-    strokeWeight(120)
-    stroke(colourOpacity(theme[3], 0.35))
-    topLine()
-    stroke(colourOpacity(theme[4], 0.3))
-    topRightLine()
-    stroke(colourOpacity(theme[3], 0.3))
-    bottomRightLine()
-    stroke(colourOpacity(theme[3], 0.35))
-    bottomLine()
-    stroke(colourOpacity(theme[4], 0.3))
-    bottomLeftLine()
-    stroke(colourOpacity(theme[3], 0.3))
-    topLeftLine()
-
-    // fat theme[2] outer lines
-    msPerLine = 500
-    stroke(colourOpacity(theme[2], 0.3))
-    strokeWeight(200)
-    horizontalLines(hexagonHeight)
-    forwardSlashLines(2.5*hexagonHeight)
-    backwardSlashLines(2.5*hexagonHeight)
-
-    // fat theme[4] / purple outer lines (goes around the inner most lines)
-    msPerLine = 600
-    stroke(colourOpacity(theme[3], 0.3))
-    bottomLeftLine(-hexagonHeight)
-    topRightLine(hexagonHeight)
-    stroke(colourOpacity(theme[4], 0.1))
-    topLeftLine(-hexagonHeight)
-    bottomRightLine(hexagonHeight)
-
-    // dark purple paired lines on outermost lines
-    msPerLine = 600
-    stroke(colourOpacity(theme[1], .75))
-    strokeWeight(10)
-    horizontalLines(hexagonHeight - 40)
-    horizontalLines(hexagonHeight + 40)
-    msPerLine = 300
-    topRightLine(2*hexagonHeight + 40)
-    topRightLine(2*hexagonHeight - 40)
-    bottomRightLine(2*hexagonHeight + 40)
-    bottomRightLine(2*hexagonHeight - 40)
-    topLeftLine(-2*hexagonHeight + 40)
-    topLeftLine(-2*hexagonHeight - 40)
-    bottomLeftLine(-2*hexagonHeight + 40)
-    bottomLeftLine(-2*hexagonHeight - 40)
-
-    // middle layer bright theme[4] lines
-    msPerLine = 300
-    stroke(colourOpacity(theme[3], .6))
-    strokeWeight(10)
-    bottomRightLine(hexagonHeight, 50)
-    bottomLeftLine(-hexagonHeight, -50)
-    topLeftLine(-hexagonHeight, -50)
-    topRightLine(hexagonHeight, 50)
-
-    // outermost theme[4] lines
-    msPerLine = 200
-    stroke(colourOpacity(theme[4], 0.4))
-    strokeWeight(15)
-    horizontalLines(hexagonHeight + 150)
-    forwardSlashLines(2.5*hexagonHeight + 300)
-    backwardSlashLines(2.5*hexagonHeight + 300)
-
-    // final layer of white lines
-    msPerLine = 600
-    strokeWeight(5)
-    stroke(colourOpacity(white, 0.6))
-    forwardSlashLines()
-    backwardSlashLines()
-    strokeWeight(10)
-    bottomRightLine(hexagonHeight, 0)
-    bottomLeftLine(-hexagonHeight, 0)
-    topLeftLine(-hexagonHeight, 0)
-    topRightLine(hexagonHeight, 0)
+    drawTwoLines(horizontalLines, diagonalLines, distanceFromCenter, offset, distanceFromCenter, offset)
   }
 
   function randomCenterHexagon() {
     let style = rand(0, 1)
-    strokeWeight(rand(10, 200))
+    strokeWeight(rand(20, 100)*numLinesScale)
     let col1 = chooseRand(theme)
     let col2 = chooseRand(theme)
     let col3 = chooseRand(theme)
-    let opac1 = randfloat(0.1, 1.0)
-    let opac2 = randfloat(0.1, 1.0)
-    let opac3 = randfloat(0.1, 1.0)
+    let opac1 = randfloat(0.5, 1.0)
+    let opac2 = randfloat(0.5, 1.0)
+    let opac3 = randfloat(0.5, 1.0)
     if (style == 0) { // lines one at a time
       stroke(colourOpacity(col1, opac1))
       topLine()
@@ -270,41 +215,54 @@ function bestaplaid(time) {
     theme = chooseRand(themes)
     background(chooseRand(theme))
 
-    msPerLine = 100
+    msPerLine = rand(50/speed, 500/speed)
 
     randomCenterHexagon()
-
-    for (let i = 0; i < rand(7, 29); i++) {
-      numLines = rand(1, 4)
+    // stroke(black)
+    // strokeWeight(100)
+    // horizontalLines(50)
+    highlightColour = chooseRand(theme)
+    for (let i = 0; i < rand(3,6)*numLinesScale; i++) {
+      numLines = rand(1, 3)
 
       colours = []
-      if (i > 0 && i % 7) { // every six, do a set of black or white
-        col = chooseRand([black, white])
+      if (chooseRand([true, false])) { // multicolour
+        for (let j = 0; j < numLines; j++)
+          colours.push(chooseRand(theme))
+      } else {
+        col = chooseRand(theme)
         for (let j = 0; j < numLines; j++)
           colours.push(col)
-      } else {
-        if (chooseRand([true, false])) { // multicolour
-          for (let j = 0; j < numLines; j++)
-            colours.push(chooseRand(theme))
-        } else {
-          col = chooseRand(theme)
-          for (let j = 0; j < numLines; j++)
-            colours.push(col)
-        }
       }
 
       lineStyle = chooseRand(lineStyles)
 
       for (let j = 0; j < numLines; j++) {
-        msPerLine = rand(100, 700)
+        msPerLine = rand(50/speed, 500/speed)
         colour = colourOpacity(colours[j], randfloat(0.1, 0.8))
-        weight = rand(5, 150)
-        if (colours[j] == black || colours[j] == white) { // give black and white a higher opacity and lower weight
-          colour = colourOpacity(colours[j], randfloat(0.6, 0.9))
-          weight = rand(3, 15)
-        }
+        weight = rand(2, 40) * numLinesScale
 
-        distance = rand(1, 4 * hexagonHeight)
+        distance = randfloat(1, maxDistance)
+        offset = rand(-70, 70) * chooseRand([0, 0, 1])
+
+        stroke(colour)
+        strokeWeight(weight)
+        lineStyle(distance, offset)
+      }
+    }
+
+
+    // highlight final layer
+    for (let i = 0; i < rand(1, 2)*numLinesScale; i++) {
+
+      lineStyle = chooseRand(lineStyles)
+
+      for (let j = 0; j < numLines; j++) {
+        msPerLine = rand(50/speed, 500/speed)
+        colour = colourOpacity(highlightColour, randfloat(0.7, 1))
+        weight = rand(3, 4) * numLinesScale
+
+        distance = randfloat(1, maxDistance)
         offset = rand(-70, 70) * chooseRand([0, 0, 1])
 
         stroke(colour)
